6632f464ce3dcc0b680563f701e2c94044832168
Miguel Silva <miguel.sva@gmail.com>
2017-07-29 19:02:42 +0100

    Initial commit

---
+++ bin.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+// TODO: Use async
+const { execSync } = require('child_process');
+
+// TODO: Move this to deptree.js
+function getPackages() {
+  try {
+    const allPackages = execSync('npm ls --json --production=false --depth=1').toString();
+    // TODO: Use --production
+    // TODO: Filter packages
+    return JSON.parse(allPackages).dependencies;
+  } catch (error) {
+    console.error('Error getting packages', error);
+  }
+  return null;
+}
+
+function getDependencies(packages) {
+  return Object.keys(packages).map((name) => {
+    const pkg = { name };
+    if (packages[name].dependencies) {
+      pkg.dependencies = getDependencies(packages[name].dependencies);
+    }
+    return pkg;
+  });
+}
+
+function flattenDependencies(dependencies) {
+  return dependencies.reduce((a, b) => a.concat(
+    (b.dependencies ? [b].concat(flattenDependencies(b.dependencies)) : b))
+  , []);
+}
+
+function getDependenciesLinks(dependencies) {
+  
+}
+
+function createGraphData(dependencies) {
+  const nodes = flattenDependencies(dependencies).map(d => ({ id: d.name }));
+  // const links = getDependenciesLinks(dependencies);
+  return { nodes };
+}
+
+function init() {
+  const packages = getPackages();
+  if (!packages) {
+    console.error('No packages found. Did you run npm i?');
+  }
+  const dependencies = getDependencies(packages);
+  console.log(JSON.stringify(createGraphData(dependencies)));
+}
+
+init();
